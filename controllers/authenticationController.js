93cf59899256e4df58f125c5ca0a76ee1751a28f
Vagelis Giannakosian <e.giannakosian@gmail.com>
2019-11-04 21:44:40 +0200

    implemented dispatch of jwt cookie along with token on createSendToken function

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -17,6 +17,22 @@ const signToken = id => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      new Date().getTime() +
+      new Date().getTimezoneOffset() + //utc offset
+        process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000 //expires after x days minutes
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production')
+    cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
