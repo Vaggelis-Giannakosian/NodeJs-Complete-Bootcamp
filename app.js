67a65ff98950f30ee2cfbf3ab4d8a67f10bd16da
Vagelis Giannakosian <e.giannakosian@gmail.com>
2019-11-04 22:22:30 +0200

    installed express-rate-limit and limited the api endpoints to 100 calls per hour from same ip

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const app = express();
@@ -7,10 +8,19 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 
-//1) Middlewares
+//1) GLOBAL Middlewares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, //1hour
+  message:
+    'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter);
 
 app.use(express.json());
 
