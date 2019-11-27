a1974ebaacd2ba9a8e058bdeaa1fbda83e2a096d
Vagelis Giannakosian <e.giannakosian@gmail.com>
2019-11-27 22:09:31 +0200

    almost completed single tour page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -5,8 +5,6 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   //1) Get all tour database
   const tours = await Tour.find();
 
-  //2)Build template
-
   //3) Render that template using tour data from step 1
   res.status(200).render('overview', {
     title: 'All Tours',
@@ -14,8 +12,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  //1) Get tours
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate(
+    {
+      path: 'reviews',
+      fields: 'review rating user'
+    }
+  );
+
+  //2) Render the template
   res.status(200).render('tour', {
-    title: 'The Forest Hiker'
+    title: 'The Forest Hiker',
+    tour
   });
-};
+});
